ce9d09e767f688eca588e7a742ff816d88e26a37
rishabhkumar <rishabhk@tekion.com>
2024-04-25 15:30:29 +0530

    setup

---
+++ checkMate/src/test/java/com/app/checkmate/CheckMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.checkmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
